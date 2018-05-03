47f4500929b0c45078831fbf75c6e8674a8dda64
Johnson Zhang <Johnson9009@163.com>
2018-05-04 00:19:16 +0800

    Refactor Test Code of "insert" of Linked List
    1. Extract "ListTest" as the base class and implement all the fixtures and help
       functions in it, all of "EmptyListTest", "OneNodeListTest" and
       "MultiNodeListTest" are derived classes of "ListTest" with different original
       lengths.
    2. Change the static variable "original_length_" to class member variable, so it
       can be overwritten in derived classes.

--- src/list/insert/insert_test.cc
+++ src/list/insert/insert_test.cc
@@ -19,9 +19,17 @@
 
 #include "gtest/gtest.h"
 
-class EmptyListTest : public testing::Test {
+class ListTest : public testing::Test {
  public:
-  EmptyListTest() : head_(nullptr) {}
+  explicit ListTest(int length) : head_(nullptr), original_length_(length) {}
+  static void CreateList(LinkNode **head, int length) {
+    LinkNode **node = head;
+    for (int i = 0; i < length; ++i) {
+      *node = new LinkNode(i, nullptr);
+      node = &((*node)->next);
+    }
+    return;
+  }
   static void DestroyList(LinkNode **head) {
     LinkNode *node = *head;
     while (node != nullptr) {
@@ -32,34 +40,26 @@ class EmptyListTest : public testing::Test {
     *head = nullptr;
     return;
   }
-  static LinkNode const * GetNode(LinkNode const *head, int pos) {
-    LinkNode const *node = head;
-    for (int i = 0; i < pos; ++i) {
-      if (node == nullptr) {
-        return nullptr;
-      }
-      node = node->next;
-    }
-    return node;
-  }
 
  protected:
   virtual void SetUp() {
     ASSERT_EQ(nullptr, head_);
-    head_ = nullptr;
-    return;
+    CreateList(&head_, original_length_);
   }
   virtual void TearDown() {
     DestroyList(&head_);
     return;
   }
-  LinkNode const * GetNode(int pos) const {
-    return GetNode(head_, pos);
-  }
   // If all tests are run serially, this variable can be static, because "SetUp" and "TearDown" can
   // ensure its consistency. But if all tests may be run in parallel, then this variable must not be
   // static.
   LinkNode *head_;
+  int original_length_;
+};
+
+class EmptyListTest : public ListTest {
+ public:
+  EmptyListTest() : ListTest(0) {}
 };
 
 TEST_F(EmptyListTest, PosEq0) {
@@ -93,29 +93,46 @@ TEST_F(EmptyListTest, InvalidPosNe0) {
   }
 }
 
-class NonEmptyListTest : public EmptyListTest {
+class OneNodeListTest : public ListTest {
  public:
-  static int GetOriginalLength() {
-    return original_length_;
-  }
-  static void CreateList(LinkNode **head) {
-    LinkNode **node = head;
-    for (int i = 0; i < original_length_; ++i) {
-      *node = new LinkNode(i, nullptr);
-      node = &((*node)->next);
-    }
-    return;
-  }
-
- protected:
-  virtual void SetUp() {
-    ASSERT_EQ(nullptr, head_);
-    CreateList(&head_);
-  }
-  static int original_length_;
+  OneNodeListTest() : ListTest(1) {}
 };
 
-int NonEmptyListTest::original_length_ = 10;
+TEST_F(OneNodeListTest, PosEq0) {
+  ASSERT_NE(nullptr, head_);
+  insert(&head_, 0, 809);
+  ASSERT_NE(nullptr, head_);
+  EXPECT_EQ(809, head_->value);
+  LinkNode const *node = head_->next;
+  ASSERT_NE(nullptr, node);
+  EXPECT_EQ(0, node->value);
+  EXPECT_EQ(nullptr, node->next);
+}
+
+TEST_F(OneNodeListTest, PosEqLength) {
+  ASSERT_NE(nullptr, head_);
+  insert(&head_, original_length_, 810);
+  ASSERT_NE(nullptr, head_);
+  EXPECT_EQ(0, head_->value);
+  LinkNode const *inserted_node = head_->next;
+  ASSERT_NE(nullptr, inserted_node);
+  EXPECT_EQ(810, inserted_node->value);
+  EXPECT_EQ(nullptr, inserted_node->next);
+}
+
+TEST_F(OneNodeListTest, InvalidPosLt0orGtLength) {
+  ASSERT_NE(nullptr, head_);
+  for (auto pos : {-23, -1, original_length_ + 1, original_length_ + 3}) {
+    EXPECT_THROW_STREQ(insert(&head_, pos, 811), std::invalid_argument, "nonempty:pos");
+  }
+}
+
+#define MULTI_NODE_CNT 10
+
+class MultiNodeListTest : public ListTest {
+ public:
+  MultiNodeListTest() : ListTest(MULTI_NODE_CNT) {}
+};
 
 // ASSERT_anything only can be used in functions whose return type is "void", because the method it
 // used to break the execution of functions is return, so macro should be used in order to break the
@@ -129,37 +146,38 @@ int NonEmptyListTest::original_length_ = 10;
     }                                                 \
   } while (false)
 
-TEST_F(NonEmptyListTest, PosEq0) {
+TEST_F(MultiNodeListTest, PosEq0) {
   ASSERT_NE(nullptr, head_);
-  insert(&head_, 0, 809);
+  insert(&head_, 0, 812);
   ASSERT_NE(nullptr, head_);
-  EXPECT_EQ(809, head_->value);
+  EXPECT_EQ(812, head_->value);
   LinkNode const *node = head_->next;
   CHECK_PARTIAL_LIST(node, 0, original_length_);
   EXPECT_EQ(nullptr, node);
 }
 
-class NonEmptyListParamTest : public testing::TestWithParam<int> {
+class MultiNodeListParamTest : public testing::TestWithParam<int> {
  public:
-  NonEmptyListParamTest() : head_(nullptr) {}
+  MultiNodeListParamTest() : head_(nullptr), original_length_(MULTI_NODE_CNT) {}
  protected:
   virtual void SetUp() {
     ASSERT_EQ(nullptr, head_);
-    NonEmptyListTest::CreateList(&head_);
+    ListTest::CreateList(&head_, original_length_);
   }
   virtual void TearDown() {
-    EmptyListTest::DestroyList(&head_);
+    ListTest::DestroyList(&head_);
   }
   LinkNode *head_;
+  int original_length_;
 };
 
-TEST_P(NonEmptyListParamTest, PosGt0LtLength) {
+TEST_P(MultiNodeListParamTest, PosGt0LtLength) {
   ASSERT_NE(nullptr, head_);
   int pos = GetParam();
   ASSERT_GT(pos, 0);
-  ASSERT_LT(pos, NonEmptyListTest::GetOriginalLength());
+  ASSERT_LT(pos, original_length_);
 
-  int insert_value = 810 + pos - 1;
+  int insert_value = 813 + pos - 1;
   insert(&head_, pos, insert_value);
   LinkNode const *node = head_;
   CHECK_PARTIAL_LIST(node, 0, pos);
@@ -167,27 +185,26 @@ TEST_P(NonEmptyListParamTest, PosGt0LtLength) {
   ASSERT_NE(nullptr, inserted_node);
   EXPECT_EQ(insert_value, inserted_node->value);
   node = inserted_node->next;
-  CHECK_PARTIAL_LIST(node, pos, (NonEmptyListTest::GetOriginalLength() - pos));
+  CHECK_PARTIAL_LIST(node, pos, (original_length_ - pos));
   EXPECT_EQ(nullptr, node);
 }
 
-INSTANTIATE_TEST_CASE_P(, NonEmptyListParamTest,
-                        testing::Range(1, NonEmptyListTest::GetOriginalLength()));
+INSTANTIATE_TEST_CASE_P(, MultiNodeListParamTest, testing::Range(1, MULTI_NODE_CNT));
 
-TEST_F(NonEmptyListTest, PosEqLength) {
+TEST_F(MultiNodeListTest, PosEqLength) {
   ASSERT_NE(nullptr, head_);
-  insert(&head_, original_length_, 811);
+  insert(&head_, original_length_, 814);
   LinkNode const *node = head_;
   CHECK_PARTIAL_LIST(node, 0, original_length_);
   LinkNode const *inserted_node = node;
   ASSERT_NE(nullptr, inserted_node);
-  EXPECT_EQ(811, inserted_node->value);
+  EXPECT_EQ(814, inserted_node->value);
   EXPECT_EQ(nullptr, inserted_node->next);
 }
 
-TEST_F(NonEmptyListTest, InvalidPosLt0orGtLength) {
+TEST_F(MultiNodeListTest, InvalidPosLt0orGtLength) {
   ASSERT_NE(nullptr, head_);
   for (auto pos : {-23, -1, original_length_ + 1, original_length_ + 3}) {
-    EXPECT_THROW_STREQ(insert(&head_, pos, 812), std::invalid_argument, "nonempty:pos");
+    EXPECT_THROW_STREQ(insert(&head_, pos, 815), std::invalid_argument, "nonempty:pos");
   }
 }
