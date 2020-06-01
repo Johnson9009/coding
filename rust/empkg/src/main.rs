4258b1ffdb9d56c2c7efd20c7ffe778f4ddbc655
Johnson Zhang <johnson@Johnsons-iMac.local>
2020-06-01 23:08:30 +0800

    Add Git Ignore File of Rust and Finish Directory listing

--- rust/empkg/src/main.rs
+++ rust/empkg/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+
+fn main() -> std::io::Result<()> {
+  for entry in std::fs::read_dir("/Users/johnson/.emacs.d/elpa/25.3/develop")? {
+    let entry = entry?;
+    if entry.file_type()?.is_dir() == true {
+      println!("dir: {:?}", entry.file_name());
+    }
+  }
+  Ok(())
 }
