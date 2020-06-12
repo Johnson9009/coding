686615af58130f33d8beac7e6ab5d5873315ff12
Johnson9009 <johnson9009@163.com>
2020-06-12 18:06:38 +0800

    [Rust] Read YAML Format Emacs Package Describe File Correct

--- rust/empkg/src/main.rs
+++ rust/empkg/src/main.rs
@@ -1,12 +1,14 @@
+use std::{env, fs};
+use yaml_rust::YamlLoader;
 
 fn main() {
-  let pkg_path = std::env::args()
+  let pkg_path = env::args()
                  .nth(1)
                  .expect("The path of emacs package directory must be given.");
 
   let excludes = vec![String::from("archives")];
   let mut packages = Vec::new();
-  for entry in std::fs::read_dir(pkg_path).unwrap() {
+  for entry in fs::read_dir(pkg_path).unwrap() {
     let entry = entry.unwrap();
     if entry.file_type().unwrap().is_dir() == true {
       let dir_name = entry.file_name().into_string().unwrap();
@@ -17,4 +19,13 @@ fn main() {
     }
   }
   println!("packages: {:#?}", packages);
+
+  let pkg_file = env::args()
+                 .nth(2)
+                 .expect("The path of emacs package describe file must be set.");
+  let yaml_string = fs::read_to_string(pkg_file).unwrap();
+  let docs = YamlLoader::load_from_str(&yaml_string).unwrap();
+  let doc = &docs[0];
+
+  println!("{:?}", doc);
 }
